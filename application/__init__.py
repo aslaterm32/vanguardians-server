b1e4de2754518b73b732b99158dc4427c6a0ec22
aslaterm32 <aslaterm32@gmail.com>
2023-09-25 11:29:17 +0100

    changed to flask

---
+++ application/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = ""
+db = SQLAlchemy(app)
+
+from application import routes
---
+++ application/models.py
@@ -0,0 +1,3 @@
+from application import db, app
+
+app.app_context().push()
