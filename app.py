af7d7586384b3811378ca4dc44b3635a0f153b6e
Robbie Stamp <stamprobbie@gmail.com>
2023-09-25 14:55:15 +0100

    removing unneccessary stuff I added to app.py by accident

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
 from application import db
-from application.models import Character
-
-
-db.drop_all()
-print("Dropping database")
-db.create_all()
-print("Creating database")
-
-print("Seeding database")
-
-db.session.commit()
