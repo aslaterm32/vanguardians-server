735d7ce69f1d06c52fcf8edcb528ac82a7f3fa83
aslaterm32 <aslaterm32@gmail.com>
2023-09-28 14:36:22 +0100

    changed requirements

--- application/models.py
+++ application/models.py
@@ -3,7 +3,7 @@ from application import db, app
 app.app_context().push()
 
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(500), nullable=False)
