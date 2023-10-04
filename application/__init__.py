974dc943e31f1b34880a015a1780397fbebc0676
DuncanCaplan <duncan.caplan@outlook.com>
2023-10-04 11:27:46 +0100

    merge

--- application/__init__.py
+++ application/__init__.py
@@ -9,7 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 app.json_provider_class.sort_keys = False
 CORS(
-    app  # , resources={r"/users": {"origins": "http://localhost:5173"}}, supports_credentials=True  <-- Uncomment for deployed repo
+    app, supports_credentials=True 
 )
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["SQLALCHEMY_DATABASE_URI"]
